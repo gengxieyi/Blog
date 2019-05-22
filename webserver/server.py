64f75df0c6490b0fa8e4af8049a6b27e474ca9c9
root <root@localhost.localdomain>
2019-05-22 10:47:30 -0400

    add remove article cmd

--- webserver/server.py
+++ webserver/server.py
@@ -44,6 +44,11 @@ def update():
     os.system("bash /mnt/webdata/Blog/webserver/script/update.sh")
     return ""
 
+@app.route('/remove/<name>',methods=['POST'])
+def remove():
+    os.remove("/mnt/webfile/" + name + ".md")
+    return ""
+
 def preview(name):
     md_text = open("/mnt/webfile/" + name + ".md").read()
     html = markdown.markdown(md_text)
